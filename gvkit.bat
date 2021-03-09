a9e36a7ef1ad901dc8ff5dcf650a4a40d1c98e6f
FNut <63280589+FNut@users.noreply.github.com>
2021-03-09 16:20:28 +0300

    Add files via upload

--- gvkit.bat
+++ gvkit.bat
@@ -5,10 +5,14 @@ echo 2) API
 echo 3) Web Client
 echo 4) About
 set /p sel="Select Num: "
-if %sel%==1 explorer https://godville.net
+if %sel%==1 goto web
 if %sel%==2 goto api
 if %sel%==3 goto webclient
 if %sel%==4 goto about
+:web
+cls
+explorer https://godville.net
+exit
 :api
 cls
 echo 1) Foinution
@@ -18,17 +22,20 @@ set /p apisel="Select Num: "
 if %apisel%==1 explorer https://godville.net/gods/api/foinution/c0f760f2eeeb
 if %apisel%==2 goto apino
 if %apisel%==3 goto apiyes
+exit
 :apino
 cls
 set /p name="Name: "
 set site=https://godville.net/gods/api/%name%
 explorer %site%
+exit
 :apiyes
 cls
 set /p name="Name: "
 set /p api="API: "
 set site=https://godville.net/gods/api/%name%/%api%
 explorer %site%
+exit
 :webclient
 cls
 echo 1) News
@@ -50,8 +57,10 @@ if %websel%==6 explorer https://godville.net/forums
 if %websel%==7 explorer https://godville.net/blog
 if %websel%==8 explorer https://wiki.godville.net
 if %websel%==9 explorer https://godville.net/help
+exit
 :about
 cls
 echo 1) GitHub
 set /p aboutsel="Select Num: "
 if %aboutsel%==1 explorer https://github.com/fnut
+exit
